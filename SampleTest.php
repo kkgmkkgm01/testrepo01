d394501ed2acd737fff473fc92a604c59c90781e
kkgmkkgm01 <kkgmkkgm01@gmail.com>
2018-12-18 18:16:55 +0900

    no message

--- SampleTest.php
+++ SampleTest.php
@@ -2,18 +2,10 @@
 require_once('./Sample.php');
 class SampleTest extends PHPUnit_Framework_TestCase
 {
-    public function testハローワールド()
+    public function test()
     {
         $Sample = new Sample();
         $this->assertEquals($Sample->helloWorld(), 'hello world!');
     }
 
-    /**
-     * @test
-     */
-    public function aaa()
-    {
-        $Sample = new Sample();
-        $this->assertEquals($Sample->helloWorld(), 'aaaaa');
-    }
 }
